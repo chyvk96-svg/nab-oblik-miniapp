dc31b7ca590682e08f678beaeeb83d386a1e21ea
chyvk96-svg <chyvk96@gmail.com>
2026-09-17 21:02:18 +0300

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,6 @@
+// ---------- Конфігурація проєкту ----------
+// Тут зберігаються публічні налаштування підключення до Supabase.
+// Publishable key безпечно тримати у відкритому коді фронтенду.
+
+const SUPABASE_URL = 'https://ypnfujxcwgfjmewrsqus.supabase.co';
+const SUPABASE_KEY = 'sb_publishable_MJXHjWXDkky9Xhvh7i-Viw_Q4Gedb8r';
